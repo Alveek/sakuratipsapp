e58f7b4d942a42b152e1cbd77ae83fae9f48b4ad
Alveek <alveek@gmail.com>
2021-04-01 17:07:50 +0300

    added yandex metric another fix

--- pages/_app.js
+++ pages/_app.js
@@ -10,11 +10,20 @@ import Footer from "../components/Footer";
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider theme={theme}>
-			<Head>
-				<meta name="yandex-verification" content="0499116b97024f38" />
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
+			<Nav />
+
+			<Container
+				className="App"
+				as="main"
+				maxW="960px"
+				px={{ base: 5, md: 8 }}
+				minH={{ base: "calc(100vh - 115px)", md: "calc(100vh - 136px)" }}
+			>
+				<Head>
+					<meta name="yandex-verification" content="0499116b97024f38" />
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
 				<!-- Yandex.Metrika counter -->
 				(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -28,24 +37,15 @@ function MyApp({ Component, pageProps }) {
    });
 					 <!-- /Yandex.Metrika counter -->
 				`,
-					}}
-				/>
-				<noscript
-					dangerouslySetInnerHTML={{
-						__html: `
+						}}
+					/>
+					<noscript
+						dangerouslySetInnerHTML={{
+							__html: `
 						<div><img src="https://mc.yandex.ru/watch/75024562" style="position:absolute; left:-9999px;" alt="" /></div>`,
-					}}
-				/>
-			</Head>
-			<Nav />
-
-			<Container
-				className="App"
-				as="main"
-				maxW="960px"
-				px={{ base: 5, md: 8 }}
-				minH={{ base: "calc(100vh - 115px)", md: "calc(100vh - 136px)" }}
-			>
+						}}
+					/>
+				</Head>
 				<Box mt={{ base: "20px", md: "20px" }} pb={8}>
 					<Component {...pageProps} />
 				</Box>
