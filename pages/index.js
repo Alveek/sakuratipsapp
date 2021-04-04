d38b8c10bb47c5126c1bf40f97ec9190c9e18b74
Alveek <alveek@gmail.com>
2021-04-04 10:10:26 +0300

    removed bg on the list

--- pages/index.js
+++ pages/index.js
@@ -17,32 +17,17 @@ export default function Home() {
 				Podcast Episodes:
 			</Heading>
 			<NextLink href="/episodes/episode_150_cycling">
-				<Link
-					p={2}
-					bg="yellow.50"
-					display="block"
-					fontSize={{ base: "20px", md: "20px" }}
-				>
+				<Link p={2} display="block" fontSize={{ base: "20px", md: "20px" }}>
 					150. サイクリング Cycling
 				</Link>
 			</NextLink>
 			<NextLink href="/episodes/episode_151">
-				<Link
-					p={2}
-					bg="purple.50"
-					display="block"
-					fontSize={{ base: "20px", md: "20px" }}
-				>
+				<Link p={2} display="block" fontSize={{ base: "20px", md: "20px" }}>
 					151. コロッケ Croquette
 				</Link>
 			</NextLink>
 			<NextLink href="/episodes/episode_152">
-				<Link
-					p={2}
-					bg="yellow.50"
-					display="block"
-					fontSize={{ base: "20px", md: "20px" }}
-				>
+				<Link p={2} display="block" fontSize={{ base: "20px", md: "20px" }}>
 					152. イケア IKEA
 				</Link>
 			</NextLink>
